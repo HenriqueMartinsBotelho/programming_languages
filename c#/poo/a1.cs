6a77282edc43ae951b475661ee45f6c3b3e15b80
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2023-09-10 08:45:34 -0300

    c# estudos

---
+++ c#/poo/a1.cs
@@ -0,0 +1,19 @@
+using System; 
+
+
+class A1{
+    static void Main(){
+        C1 obj1 = new C1();
+        Console.WriteLine(obj1.Metodo1());
+    }
+
+}
+
+class C1{
+    int n1 = 20;
+
+    public void Metodo1(){
+        Console.WriteLine("Classe C1");
+    }
+
+}
